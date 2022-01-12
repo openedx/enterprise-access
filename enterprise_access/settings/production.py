54291411493d900a119f7b4ec4c571928e690b66
Chris Pappas <cpappas@edx.org>
2022-01-12 15:47:01 -0500

    fix: attempt to fix yaml load error

--- enterprise_access/settings/production.py
+++ enterprise_access/settings/production.py
@@ -24,7 +24,7 @@ FILE_STORAGE_BACKEND = {}
 if 'ENTERPRISE_ACCESS_CFG' in environ:
     CONFIG_FILE = get_env_setting('ENTERPRISE_ACCESS_CFG')
     with open(CONFIG_FILE, encoding='utf-8') as f:
-        config_from_yaml = yaml.load(f)
+        config_from_yaml = yaml.safe_load(f)
 
         # Remove the items that should be used to update dicts, and apply them separately rather
         # than pumping them into the local vars.
