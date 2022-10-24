d28396316d24dfa5a9adf1186345862ff4567c4f
muhammad-ammar <mammar@gmail.com>
2022-10-24 12:04:17 +0500

    fix: disable wrong unsupported-binary-operation

--- enterprise_access/apps/core/rules.py
+++ enterprise_access/apps/core/rules.py
@@ -46,7 +46,7 @@ def has_explicit_access_to_requests_admin(user, enterprise_customer_uuid):
     )
 
 
-has_admin_access = has_implicit_access_to_requests_admin | has_explicit_access_to_requests_admin
+has_admin_access = has_implicit_access_to_requests_admin | has_explicit_access_to_requests_admin  # pylint: disable=unsupported-binary-operation
 
 rules.add_perm(
     constants.REQUESTS_ADMIN_ACCESS_PERMISSION,
@@ -92,7 +92,7 @@ def has_explicit_access_to_requests_learner(user, enterprise_customer_uuid):
     )
 
 
-has_learner_access = has_implicit_access_to_requests_learner | has_explicit_access_to_requests_learner
+has_learner_access = has_implicit_access_to_requests_learner | has_explicit_access_to_requests_learner  # pylint: disable=unsupported-binary-operation
 
 # Grants access permission if the user is a learner or admin
 rules.add_perm(
