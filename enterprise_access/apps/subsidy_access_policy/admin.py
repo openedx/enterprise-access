c5ca7d37a929c8a4344727b521c56642501f854d
Alex Dusenbery <adusenbery@edx.org>
2023-06-08 10:41:27 -0400

    feat: various policy admin improvements
    * Uses DjangoQL for policy list display
    * Creates policy formsets to split base configuration section from spend limit section
    * Adds read-only limit fields denoted as dollars

--- enterprise_access/apps/subsidy_access_policy/admin.py
+++ enterprise_access/apps/subsidy_access_policy/admin.py
@@ -1,10 +1,18 @@
 """ Admin configuration for subsidy_access_policy models. """
 
-
 from django.contrib import admin
 from django.utils.text import Truncator  # for shortening a text
+from djangoql.admin import DjangoQLSearchMixin
 
-from enterprise_access.apps.subsidy_access_policy import models
+from enterprise_access.apps.subsidy_access_policy import constants, models
+
+
+def cents_to_usd_string(cents):
+    """
+    Helper to convert cents as an int to dollars as a
+    nicely formatted string.
+    """
+    return "${:,.2f}".format(float(cents) / constants.CENTS_PER_DOLLAR)
 
 
 class BaseSubsidyAccessPolicyMixin(admin.ModelAdmin):
@@ -16,11 +24,38 @@ class BaseSubsidyAccessPolicyMixin(admin.ModelAdmin):
         'modified',
         'active',
         'short_description',
-        'spend_limit',
+        'policy_spend_limit_dollars',
     )
     list_filter = (
         'active',
         'access_method',
+    )
+    ordering = ['-modified']
+
+    readonly_fields = (
+        'created',
+        'modified',
+        'policy_spend_limit_dollars',
+    )
+
+    def short_description(self, obj):
+        return Truncator(str(obj.description)).chars(255)
+
+    @admin.display(description='Policy-wide spend limit (dollars)')
+    def policy_spend_limit_dollars(self, obj):
+        """Returns this policy's spend_limit as a US Dollar string."""
+        if obj.spend_limit is None:
+            return None
+        return cents_to_usd_string(obj.spend_limit)
+
+
+@admin.register(models.PerLearnerEnrollmentCreditAccessPolicy)
+class PerLearnerEnrollmentCreditAccessPolicy(DjangoQLSearchMixin, BaseSubsidyAccessPolicyMixin):
+    """
+    Admin configuration for PerLearnerEnrollmentCreditAccessPolicy.
+    """
+    list_display = BaseSubsidyAccessPolicyMixin.list_display + (
+        'per_learner_enrollment_limit',
     )
     search_fields = (
         'uuid',
@@ -28,27 +63,55 @@ class BaseSubsidyAccessPolicyMixin(admin.ModelAdmin):
         'catalog_uuid',
         'subsidy_uuid',
     )
-    ordering = ['-modified']
-
-    def short_description(self, obj):
-        return Truncator(str(obj.description)).chars(255)
-
-
-@admin.register(models.PerLearnerEnrollmentCreditAccessPolicy)
-class PerLearnerEnrollmentCreditAccessPolicy(BaseSubsidyAccessPolicyMixin):
-    """
-    Admin configuration for PerLearnerEnrollmentCreditAccessPolicy.
-    """
-    list_display = BaseSubsidyAccessPolicyMixin.list_display + (
-        'per_learner_enrollment_limit',
-    )
 
 
 @admin.register(models.PerLearnerSpendCreditAccessPolicy)
-class PerLearnerSpendCreditAccessPolicy(BaseSubsidyAccessPolicyMixin):
+class PerLearnerSpendCreditAccessPolicy(DjangoQLSearchMixin, BaseSubsidyAccessPolicyMixin):
     """
     Admin configuration for PerLearnerSpendCreditAccessPolicy.
     """
     list_display = BaseSubsidyAccessPolicyMixin.list_display + (
-        'per_learner_spend_limit',
+        'per_learner_spend_limit_dollars',
     )
+    readonly_fields = BaseSubsidyAccessPolicyMixin.readonly_fields + (
+        'per_learner_spend_limit_dollars',
+    )
+    search_fields = (
+        'uuid',
+        'enterprise_customer_uuid',
+        'catalog_uuid',
+        'subsidy_uuid',
+    )
+
+    fieldsets = [
+        (
+            'Base configuration',
+            {
+                'fields': [
+                    'enterprise_customer_uuid',
+                    'description',
+                    'active',
+                    'catalog_uuid',
+                    'subsidy_uuid',
+                    'created',
+                    'modified',
+                ]
+            }
+        ),
+        (
+            'Spend limits',
+            {
+                'fields': [
+                    'spend_limit',
+                    'policy_spend_limit_dollars',
+                    'per_learner_spend_limit',
+                    'per_learner_spend_limit_dollars',
+                ]
+            }
+        ),
+    ]
+
+    @admin.display(description='Per-learner spend limit (dollars)')
+    def per_learner_spend_limit_dollars(self, obj):
+        """Returns this policy's per_learner_spend_limit as a US Dollar string."""
+        return cents_to_usd_string(obj.per_learner_spend_limit)
