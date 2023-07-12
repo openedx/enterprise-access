10b8fadf34a27ebb5d7d5141cbe5777d4338fa4f
Troy Sankey <tsankey@edx.org>
2023-07-12 18:20:16 -0400

    fix: history tracking for SubsidyAccessPolicy proxy models
    Before this commit, history tracking on SubsidyAccessPolicy and all
    child models was non-functional. Now, all history is now tracked in the
    pre-existing `historicalsubsidyaccesspolicy` table, including changes to
    all child proxy models of SubsidyAccessPolicy.
    
    At the time of writing, django-simple-history does not support tracking proxy
    models natively.  This upstream issue contains a few workarounds, and we
    incorporated one of them:
    
    https://github.com/jazzband/django-simple-history/issues/544
    
    One downside is that it creates migration clutter.  The migration file
    included in this PR contains only no-ops, literally sqlmigrate returns
    `BEGIN; COMMIT;`.  As far as I can tell, this causes no harm.
    
    NOTE: This does not retroactively track creation and update records.
    For historical integrity, we should at least consider running this
    command to give the current instances initial change records:
    
    ```
    python manage.py populate_history --auto
    ```
    
    https://django-simple-history.readthedocs.io/en/latest/quick_start.html#existing-projects
    
    ENT-7150

--- enterprise_access/apps/subsidy_access_policy/utils.py
+++ enterprise_access/apps/subsidy_access_policy/utils.py
@@ -3,9 +3,11 @@ Utils for subsidy_access_policy
 """
 import hashlib
 
+from django.apps import apps
 from django.conf import settings
 from edx_django_utils.cache import RequestCache
 from edx_enterprise_subsidy_client import get_enterprise_subsidy_api_client
+from simple_history.models import HistoricalRecords, registered_models
 
 from enterprise_access import __version__ as code_version
 
@@ -64,3 +66,74 @@ def create_idempotency_key_for_transaction(subsidy_uuid, **metadata):
     }
     hashed_metadata = hashlib.md5(str(idpk_data).encode()).hexdigest()
     return f'{LEDGERED_SUBSIDY_IDEMPOTENCY_KEY_PREFIX}-{subsidy_uuid}-{hashed_metadata}'
+
+
+class ProxyAwareHistoricalRecords(HistoricalRecords):
+    """
+    This specialized HistoricalRecords model field is to be used specifically for tracking history on instances of proxy
+    models.  Set `history = ProxyAwareHistoricalRecords(inherit=True) on the parent (concrete) model, and proxy models
+    that subclass the parent will have their history tracked in the history table of the parent.
+
+    The only downside is that this seems to cause no-op migrations to be created for each proxy model (as opposed to no
+    migration at all).  The migration defines a historical table for the proxy model with no fields, which sqlmigrate
+    translates to an empty transaction: `BEGIN; COMMIT;`.
+
+    Copied verbatim from https://github.com/jazzband/django-simple-history/issues/544#issuecomment-1538615799
+    """
+    def _find_base_history(self, opts):
+        """
+        Search for a history model in the parent models that we can re-use.
+        """
+        base_history = None
+        for parent_class in opts.parents.keys():
+            if hasattr(parent_class, 'history'):
+                base_history = parent_class.history.model
+        return base_history
+
+    def create_history_model(self, model, inherited):
+        """
+        Override super.create_history_model() to force creation of a history model if this is a proxy model.
+        """
+        opts = model._meta
+        if opts.proxy:
+            base_history = self._find_base_history(opts)
+            if base_history:
+                return self.create_proxy_history_model(model, inherited, base_history)
+
+        return super().create_history_model(model, inherited)
+
+    def create_proxy_history_model(self, model, inherited, base_history):
+        """
+        Create a history model for this proxy model, inheriting from the parent history model so that historical records
+        are tracked in the latter.
+        """
+        opts = model._meta
+        attrs = {
+            '__module__': self.module,
+            '_history_excluded_fields': self.excluded_fields,
+        }
+        app_module = f'{opts.app_label}.models'
+        if inherited:
+            attrs['__module__'] = model.__module__
+        elif model.__module__ != self.module:
+            # registered under different app
+            attrs['__module__'] = self.module
+        elif app_module != self.module:
+            # Abuse an internal API because the app registry is loading.
+            app = apps.app_configs[opts.app_label]
+            models_module = app.name
+            attrs['__module__'] = models_module
+
+        attrs.update(
+            Meta=type(
+                'Meta',
+                (),
+                {**self.get_meta_options(model), 'proxy': True}
+            )
+        )
+        if self.table_name is not None:
+            attrs['Meta'].db_table = self.table_name
+
+        name = self.get_history_model_name(model)
+        registered_models[opts.db_table] = model
+        return type(str(name), (base_history,), attrs)
