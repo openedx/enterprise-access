08ff73c5cc5b2439eaada60c88eeb7696f8c491d
Alex Dusenbery <adusenbery@edx.org>
2023-10-02 12:59:56 -0400

    feat: LearnerContentAssignmentActions model

--- enterprise_access/apps/content_assignments/admin.py
+++ enterprise_access/apps/content_assignments/admin.py
@@ -24,19 +24,50 @@ class AssignmentConfigurationAdmin(DjangoQLSearchMixin, SimpleHistoryAdmin):
     )
     list_filter = ('active',)
     ordering = ['-modified']
-    read_only_fields = (
+    readonly_fields = (
         'created',
         'modified',
     )
 
 
+class ActionInline(admin.TabularInline):
+    """
+    Inline admin for linking actions into their related assignment record.
+    """
+    model = models.LearnerContentAssignmentAction
+
+    fields = (
+        'action_type',
+        'completed_at',
+        'error_reason',
+    )
+
+    ordering = ['-modified']
+
+    show_change_link = True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('assignment')
+
+
 @admin.register(models.LearnerContentAssignment)
-class LearnerContentAssignment(DjangoQLSearchMixin, SimpleHistoryAdmin):
+class LearnerContentAssignmentAdmin(DjangoQLSearchMixin, SimpleHistoryAdmin):
     """
     Admin configuration for LearnerContentAssignments.
     """
     list_display = (
-        'assignment_configuration',
+        'uuid',
+        'get_assignment_configuration_uuid',
+        'get_enterprise_customer_uuid',
         'learner_email',
         'lms_user_id',
         'content_key',
@@ -49,12 +80,69 @@ class LearnerContentAssignment(DjangoQLSearchMixin, SimpleHistoryAdmin):
         'uuid',
         'learner_email',
         'lms_user_id',
+        'assignment_configuration',
+        'assignment_configuration__enterprise_customer_uuid',
     )
     list_filter = ('state',)
-    read_only_fields = (
+    readonly_fields = (
         'last_notification_at',
         'created',
         'modified',
         'lms_user_id',
-        'assignment_configuration',
+        'get_enterprise_customer_uuid',
     )
+    autocomplete_fields = ['assignment_configuration']
+
+    list_select_related = ('assignment_configuration',)
+
+    inlines = [ActionInline]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('assignment_configuration')
+
+    @admin.display(ordering='uuid', description='Assignment configuration UUID')
+    def get_assignment_configuration_uuid(self, obj):
+        return obj.assignment_configuration.uuid
+
+    @admin.display(ordering='enterprise_customer_uuid', description='Enterprise customer uuid')
+    def get_enterprise_customer_uuid(self, obj):
+        return obj.assignment_configuration.enterprise_customer_uuid
+
+
+@admin.register(models.LearnerContentAssignmentAction)
+class LearnerContentAssignmentActionAdmin(DjangoQLSearchMixin, SimpleHistoryAdmin):
+    """
+    Admin configuration for LearnerContentAssignmentAction.
+    """
+    list_display = (
+        'uuid',
+        'get_assignment',
+        'action_type',
+        'completed_at',
+        'error_reason',
+        'modified',
+    )
+    ordering = ['-modified']
+    search_fields = (
+        'uuid',
+        'assignment',
+        'traceback',
+    )
+    list_filter = ('action_type', 'error_reason')
+    readonly_fields = (
+        'created',
+        'modified',
+        'traceback',
+    )
+    autocomplete_fields = ['assignment']
+
+    list_select_related = ('assignment',)
+
+    @admin.display(ordering='uuid', description='Assignment UUID')
+    def get_assignment(self, obj):
+        return obj.assignment.uuid
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related(
+            'assignment',
+        )
--- enterprise_access/apps/content_assignments/constants.py
+++ enterprise_access/apps/content_assignments/constants.py
@@ -23,3 +23,32 @@ class LearnerContentAssignmentStateChoices:
 
     # States which allow cancellation by an admin.
     CANCELABLE_STATES = (ALLOCATED, ERRORED)
+
+
+class AssignmentActions:
+    """
+    Actions allowed on a given LearnerContentAssignment.
+    """
+    LEARNER_LINKED = 'learner_linked'
+    NOTIFIED = 'notified'
+    REMINDED = 'reminded'
+
+    CHOICES = (
+        (LEARNER_LINKED, 'Learner linked to customer'),
+        (NOTIFIED, 'Learner notified of assignment'),
+        (REMINDED, 'Learner reminded about assignment'),
+    )
+
+
+class AssignmentActionErrors:
+    """
+    Error reasons (like an error code) for errors encountered
+    during an assignment action.
+    """
+    EMAIL_ERROR = 'email_error'
+    INTERNAL_API_ERROR = 'internal_api_error'
+
+    CHOICES = (
+        (EMAIL_ERROR, 'Email error'),
+        (INTERNAL_API_ERROR, 'Internal API error'),
+    )
