1bab90bff57d6374d26ebdacee163f2ec955cdd3
Alex Dusenbery <adusenbery@edx.org>
2023-12-05 10:18:56 -0500

    feat: get_list_price refactoring for assigned price.
    ENT-8040 | Computes the list price dict returned by
    the `can-redeem` view from the value of the `LearnerContentAssignment.quantity`
    for assignment-based policies.

--- enterprise_access/apps/subsidy_access_policy/content_metadata_api.py
+++ enterprise_access/apps/subsidy_access_policy/content_metadata_api.py
@@ -3,7 +3,9 @@ Python API for interacting with content metadata
 for use in the domain of SubsidyAccessPolicies.
 """
 import logging
+from decimal import Decimal
 
+import requests
 from django.conf import settings
 from edx_django_utils.cache import TieredCache
 from requests.exceptions import HTTPError
@@ -11,6 +13,7 @@ from requests.exceptions import HTTPError
 from enterprise_access.cache_utils import versioned_cache_key
 
 from ..api_client.enterprise_catalog_client import EnterpriseCatalogApiClient
+from .exceptions import ContentPriceNullException
 from .utils import get_versioned_subsidy_client
 
 logger = logging.getLogger(__name__)
@@ -79,3 +82,46 @@ def get_and_cache_catalog_contains_content(enterprise_catalog_uuid, content_key,
     )
     TieredCache.set_all_tiers(cache_key, result, timeout or DEFAULT_CACHE_TIMEOUT)
     return result
+
+
+def get_list_price_for_content(enterprise_customer_uuid, content_key, content_metadata=None):
+    """
+    Given a customer and content identifier, fetch content metadata and return a list price
+    dictionary. If the caller already has a dictionary of ``content_metadata`` in scope, this
+    function computes its return value from that.
+    Returns:
+        A dictionary of the form
+        ```
+        {
+            "usd": 149.50, # the list price in US Dollars as a float
+            "usd_cents": 14950 # the list price in USD Cents as an int
+        }
+
+    Raises:
+        A ``ContentPriceNullException`` if we encountered an HTTPError fetch content metadata.
+    """
+    if not content_metadata:
+        try:
+            content_metadata = get_and_cache_content_metadata(enterprise_customer_uuid, content_key)
+        except requests.exceptions.HTTPError as exc:
+            logger.warning(
+                f'{exc} when checking content metadata for {enterprise_customer_uuid} and {content_key}'
+            )
+            raise ContentPriceNullException(f'Could not determine list price for {content_key}') from exc
+
+    # Note that the "content_price" key is guaranteed to exist, but the value may be None.
+    return list_price_dict_from_usd_cents(content_metadata['content_price'])
+
+
+def list_price_dict_from_usd_cents(list_price_integer_cents):
+    """
+    Helper to compute a list price dictionary given the non-negative price of the content in USD cents.
+    """
+    list_price_decimal_dollars = None
+    if list_price_integer_cents is not None:
+        list_price_decimal_dollars = Decimal(list_price_integer_cents) / 100
+
+    return {
+        "usd": list_price_decimal_dollars,
+        "usd_cents": list_price_integer_cents,
+    }
