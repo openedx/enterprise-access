289d890dee065922ce34515cf07e88d40afdea76
Katrina Nguyen <71999631+katrinan029@users.noreply.github.com>
2024-06-07 11:15:46 -0700

    chore: add debugging lines (#481)

--- enterprise_access/apps/enterprise_groups/tasks.py
+++ enterprise_access/apps/enterprise_groups/tasks.py
@@ -5,6 +5,7 @@ Celery tasks for Enterprise Access API.
 import logging
 from datetime import datetime, timedelta
 
+from braze.exceptions import BrazeClientError
 from celery import shared_task
 from django.conf import settings
 
@@ -33,10 +34,20 @@ def get_braze_campaign_properties(
     invitation_end_date = datetime.strptime(
         recent_action_time, "%B %d, %Y"
     ) + timedelta(days=DAYS_TO_PURGE_PII)
+    logger.info('get_braze_campaign_properties_1: recent_action_time {%s}, '
+                'current_date {%s}, invitation_end_date {%s}, catalog_count {%s}, subsidy_expiration_datetime {%s}',
+                recent_action_time,
+                current_date,
+                invitation_end_date,
+                catalog_count,
+                subsidy_expiration_datetime,)
 
     if current_date - timedelta(
         days=BRAZE_GROUPS_EMAIL_CAMPAIGNS_FIRST_REMINDER_DAY
     ) == datetime.strptime(recent_action_time, "%B %d, %Y"):
+        logger.info('get_braze_campaign_properties_2: properties for reminder day {%s} for enterprise customer {%s}',
+                    BRAZE_GROUPS_EMAIL_CAMPAIGNS_FIRST_REMINDER_DAY,
+                    enterprise_customer_name)
         return {
             "braze_campaign_id": settings.BRAZE_GROUPS_EMAIL_AUTO_REMINDER_DAY_5_CAMPAIGN,
             # the trigger properties are different depending on the reminder email
@@ -51,6 +62,9 @@ def get_braze_campaign_properties(
     if current_date - timedelta(
         days=BRAZE_GROUPS_EMAIL_CAMPAIGNS_SECOND_REMINDER_DAY
     ) == datetime.strptime(recent_action_time, "%B %d, %Y"):
+        logger.info('get_braze_campaign_properties_3: properties for reminder day {%s} for enterprise customer {%s}',
+                    BRAZE_GROUPS_EMAIL_CAMPAIGNS_SECOND_REMINDER_DAY,
+                    enterprise_customer_name)
         return {
             "braze_campaign_id": settings.BRAZE_GROUPS_EMAIL_AUTO_REMINDER_DAY_25_CAMPAIGN,
             "braze_trigger_properties": {
@@ -63,6 +77,9 @@ def get_braze_campaign_properties(
     if current_date - timedelta(
         days=BRAZE_GROUPS_EMAIL_CAMPAIGNS_THIRD_REMINDER_DAY
     ) == datetime.strptime(recent_action_time, "%B %d, %Y"):
+        logger.info('get_braze_campaign_properties_4: properties for reminder day {%s} for enterprise customer {%s}',
+                    BRAZE_GROUPS_EMAIL_CAMPAIGNS_THIRD_REMINDER_DAY,
+                    enterprise_customer_name)
         return {
             "braze_campaign_id": settings.BRAZE_GROUPS_EMAIL_AUTO_REMINDER_DAY_50_CAMPAIGN,
             "braze_trigger_properties": {
@@ -75,6 +92,9 @@ def get_braze_campaign_properties(
     if current_date - timedelta(
         days=BRAZE_GROUPS_EMAIL_CAMPAIGNS_FOURTH_REMINDER_DAY
     ) == datetime.strptime(recent_action_time, "%B %d, %Y"):
+        logger.info('get_braze_campaign_properties_5: properties for reminder day {%s} for enterprise customer {%s}',
+                    BRAZE_GROUPS_EMAIL_CAMPAIGNS_FOURTH_REMINDER_DAY,
+                    enterprise_customer_name)
         return {
             "braze_campaign_id": settings.BRAZE_GROUPS_EMAIL_AUTO_REMINDER_DAY_65_CAMPAIGN,
             "braze_trigger_properties": {
@@ -88,6 +108,9 @@ def get_braze_campaign_properties(
     if current_date - timedelta(
         days=BRAZE_GROUPS_EMAIL_CAMPAIGNS_FINAL_REMINDER_DAY
     ) == datetime.strptime(recent_action_time, "%B %d, %Y"):
+        logger.info('get_braze_campaign_properties_6: properties for reminder day {%s} for enterprise customer {%s}',
+                    BRAZE_GROUPS_EMAIL_CAMPAIGNS_FINAL_REMINDER_DAY,
+                    enterprise_customer_name)
         return {
             "braze_campaign_id": settings.BRAZE_GROUPS_EMAIL_AUTO_REMINDER_DAY_85_CAMPAIGN,
             "braze_trigger_properties": {
@@ -129,9 +152,17 @@ def send_group_reminder_emails(pending_enterprise_users):
             pending_enterprise_user["catalog_count"],
             pending_enterprise_user["subsidy_expiration_datetime"],
         )
+    try:
         logger.info(f'Sending braze campaign group reminder email to {recipient}.')
         braze_client_instance.send_campaign_message(
             braze_properties["braze_campaign_id"],
             recipients=[recipient],
             trigger_properties=braze_properties["braze_trigger_properties"],
         )
+    except BrazeClientError as exc:
+        message = (
+            "Groups learner reminder email could not be sent "
+            f"to {recipient} with braze properties {braze_properties}."
+        )
+        logger.exception(message)
+        raise exc
