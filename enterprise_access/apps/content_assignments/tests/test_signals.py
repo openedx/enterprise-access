2ccddc00945bf18bba9e70f40b9030fd7a11cf98
Ahmad Hassan <work.ahmadhassan@gmail.com>
2025-07-09 15:25:43 +0500

    feat: set LCR status to reversed when related content assignment is reversed (#765)

--- enterprise_access/apps/content_assignments/tests/test_signals.py
+++ enterprise_access/apps/content_assignments/tests/test_signals.py
@@ -1,11 +1,24 @@
 """
 Signals and handlers tests.
 """
+from unittest import mock
+from uuid import uuid4
+
+from django.db import DatabaseError
 from django.test import TestCase
 from django.utils import timezone
 
+from enterprise_access.apps.content_assignments.constants import LearnerContentAssignmentStateChoices
+from enterprise_access.apps.content_assignments.models import LearnerContentAssignment
+from enterprise_access.apps.content_assignments.signals import update_assignment_status_for_reversed_transaction
 from enterprise_access.apps.content_assignments.tests.factories import LearnerContentAssignmentFactory
 from enterprise_access.apps.core.tests.factories import UserFactory
+from enterprise_access.apps.subsidy_request.models import (
+    LearnerCreditRequestActionErrorReasons,
+    LearnerCreditRequestActions,
+    SubsidyRequestStates
+)
+from enterprise_access.apps.subsidy_request.tests.factories import LearnerCreditRequestFactory
 
 TEST_EMAIL = 'test@example.com'
 
@@ -71,3 +84,64 @@ class SignalsTests(TestCase):
         for assignment in assignments_other_learner:
             assignment.refresh_from_db()
             assert assignment.lms_user_id is None
+
+
+class TestReversalSignal(TestCase):
+    """
+    Tests for reversal signals.
+    """
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.assignment = LearnerContentAssignmentFactory(
+            state=LearnerContentAssignmentStateChoices.ACCEPTED,
+            lms_user_id=self.user.lms_user_id,
+            transaction_uuid=uuid4()
+        )
+        self.lcr = LearnerCreditRequestFactory(
+            user=self.user,
+            assignment=self.assignment,
+            state=SubsidyRequestStates.ACCEPTED,
+        )
+        self.assignment.refresh_from_db()
+
+    def test_assignment_and_lcr_reversal_happy_path(self):
+        update_assignment_status_for_reversed_transaction(
+            ledger_transaction=mock.Mock(uuid=self.assignment.transaction_uuid)
+        )
+        self.assignment.refresh_from_db()
+        self.lcr.refresh_from_db()
+        actions = LearnerCreditRequestActions.objects.filter(learner_credit_request=self.lcr)
+        assert actions.exists(), "No LearnerCreditRequestActions created"
+        action = actions.latest('created')
+
+        assert self.assignment.state == LearnerContentAssignmentStateChoices.REVERSED
+        assert self.lcr.state == SubsidyRequestStates.REVERSED
+        assert action.recent_action == SubsidyRequestStates.REVERSED
+        assert action.status == SubsidyRequestStates.REVERSED
+        assert not action.error_reason
+
+    def test_assignment_reversal_no_lcr(self):
+        assignment = LearnerContentAssignmentFactory(
+            state=LearnerContentAssignmentStateChoices.ACCEPTED,
+            lms_user_id=self.user.lms_user_id,
+            transaction_uuid=uuid4()
+        )
+        update_assignment_status_for_reversed_transaction(
+            ledger_transaction=mock.Mock(uuid=assignment.transaction_uuid)
+        )
+        assignment.refresh_from_db()
+        assert assignment.state == LearnerContentAssignmentStateChoices.REVERSED
+        assert not LearnerCreditRequestActions.objects.filter(learner_credit_request__assignment=assignment).exists()
+
+    def test_reversal_error_updates_action(self):
+        with mock.patch.object(LearnerContentAssignment, "save", side_effect=DatabaseError("Simulated DB error")):
+            update_assignment_status_for_reversed_transaction(
+                ledger_transaction=mock.Mock(uuid=self.assignment.transaction_uuid)
+            )
+        actions = LearnerCreditRequestActions.objects.filter(learner_credit_request=self.lcr)
+        assert actions.exists(), "No LearnerCreditRequestActions created"
+        action = actions.latest('created')
+        assert action.error_reason == LearnerCreditRequestActionErrorReasons.FAILED_REVERSAL
+        assert action.status == SubsidyRequestStates.ACCEPTED
+        assert "Simulated DB error" in action.traceback
