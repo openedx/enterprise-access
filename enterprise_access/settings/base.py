ffc5f3c18983559160188fd122786147c57153cb
Kira Miller <kira.miller15@yahoo.com>
2025-09-16 20:12:55 +0000

    fix: adding learner profile permissions to operators

--- enterprise_access/settings/base.py
+++ enterprise_access/settings/base.py
@@ -343,6 +343,7 @@ SYSTEM_TO_FEATURE_ROLE_MAPPING = {
         BFF_OPERATOR_ROLE,
         PROVISIONING_ADMIN_ROLE,
         CUSTOMER_BILLING_OPERATOR_ROLE,
+        ADMIN_LEARNER_PROFILE_ADMIN_ROLE,
     ],
     SYSTEM_ENTERPRISE_ADMIN_ROLE: [
         # enterprise admins only need learner-level access to Subsidy Access Policy APIs since they aren't responsible
