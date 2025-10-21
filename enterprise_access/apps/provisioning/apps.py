ece911d042b1619cbeb20e1f63512b517bb002a4
Alexander Dusenbery <adusenbery@2u.com>
2025-10-21 13:11:20 -0400

    fix: remove provisioning app config

--- enterprise_access/apps/provisioning/apps.py
+++
@@ -1,8 +0,0 @@
-""" App config for provisioning """
-
-from django.apps import AppConfig
-
-
-class ProvisioningConfig(AppConfig):
-    """ App config for provisioning. """
-    name = 'enterprise_access.apps.provisioning'
