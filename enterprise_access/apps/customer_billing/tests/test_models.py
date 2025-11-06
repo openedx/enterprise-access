e2f536bd28562c3f62bb17a7529fc49ae0b152de
Kira Miller <31229189+kiram15@users.noreply.github.com>
2025-11-06 17:02:21 +0000

    fix: remove extra code

--- enterprise_access/apps/customer_billing/tests/test_models.py
+++ enterprise_access/apps/customer_billing/tests/test_models.py
@@ -26,32 +26,11 @@ from enterprise_access.apps.customer_billing.models import (
     StripeEventSummary
 )
 from enterprise_access.apps.customer_billing.stripe_event_handlers import StripeEventHandler
+from enterprise_access.apps.customer_billing.tests.test_stripe_event_handlers import AttrDict
 from enterprise_access.apps.provisioning.models import GetCreateSubscriptionPlanStep
 from enterprise_access.apps.provisioning.tests.factories import ProvisionNewCustomerWorkflowFactory
 
 User = get_user_model()
-
-
-class AttrDict(dict):
-    """
-    Minimal helper that allows both attribute (obj.foo) and item (obj['foo']) access.
-    Recursively converts nested dicts to AttrDicts, but leaves non-dict values as-is.
-    """
-    def __getattr__(self, name):
-        try:
-            value = self[name]
-        except KeyError as e:
-            raise AttributeError(name) from e
-        return value
-
-    def __setattr__(self, name, value):
-        self[name] = value
-
-    @staticmethod
-    def wrap(value):
-        if isinstance(value, dict) and not isinstance(value, AttrDict):
-            return AttrDict({k: AttrDict.wrap(v) for k, v in value.items()})
-        return value
 
 
 @ddt.ddt
